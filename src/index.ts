f5c0041fa654ec304515fbd48d6158fc1f66fbd8
Санталов Артур <arthur@MacBook-Pro-Santalov.local>
2025-11-09 00:18:40 +0400

    feat: add basic crud

---
+++ src/index.ts
@@ -0,0 +1,56 @@
+import http from 'node:http';
+import dotenv from 'dotenv';
+
+import path from 'node:path';
+
+import { userController } from './controllers/user.controller.ts';
+
+dotenv.config({
+  path: path.join('.', '.env', `.env.${process.env.NODE_ENV?.toLowerCase()}`),
+});
+
+const PORT = process.env.PORT;
+
+const server = http.createServer(
+  async (req: http.IncomingMessage, res: http.ServerResponse) => {
+    switch (req.method) {
+      case 'GET':
+        if (['', '/users'].includes(req.url)) {
+          await userController['getAll'](res);
+        } else if (req.url?.startsWith('/users/')) {
+          await userController['getUserById'](req, res);
+        } else {
+          res.writeHead(404).end('Error: Unhandled route');
+        }
+        return;
+      case 'POST':
+        if ('/users' === req.url) {
+          await userController['createUser'](req, res);
+        } else {
+          res.writeHead(404).end('Error: Unhandled route');
+        }
+        return;
+
+      case 'PUT':
+        if (req.url?.startsWith('/users/')) {
+          userController['updateUser'](req, res);
+        } else {
+          res.writeHead(404).end('Error: Unhandled route');
+        }
+        return;
+      case 'DELETE':
+        if (req.url?.startsWith('/users/')) {
+          userController['deleteUser'](req, res);
+        } else {
+          res.writeHead(404).end('Error: Unhandled route');
+        }
+        return;
+      default:
+        res.writeHead(404).end('Error: Unhandled route');
+    }
+  },
+);
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
---
+++ src/utils/readFile.ts
@@ -0,0 +1,21 @@
+import path from 'node:path';
+import fs from 'fs/promises';
+
+const filePath = path.join(process.cwd(), 'src', 'mock', 'users.json');
+
+export const readUsersFile = async <
+  T extends Record<string, unknown>,
+>(): Promise<{ data: T[] }> => {
+  try {
+    const data = await fs.readFile(filePath, 'utf-8');
+    return JSON.parse(data);
+  } catch (error: any) {
+    throw error;
+  }
+};
+
+export const writeUsersFile = async <T extends Record<string, unknown>>(data: {
+  data: T[];
+}): Promise<void> => {
+  await fs.writeFile(filePath, JSON.stringify(data, null, 2));
+};
